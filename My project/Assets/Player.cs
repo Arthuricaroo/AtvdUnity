55c85caff7fb4564296ba6bd21a7042332391562
Arthuricaroo <arthur.icaro@escolar.ifrn.edu.br>
2025-02-22 14:20:30 -0300

    AtualizaçãoFinal

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -7,19 +7,19 @@ public class Player : MonoBehaviour
 {
     private Transform _transform;
     private Rigidbody2D _rigidbody2D;
-    
+
     public float velocidade = 10f;
     public float forcaPulo = 10f;
 
     private bool noChao = false;
-    
+
     void Start()
     {
         _transform = gameObject.transform;
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    
+
     void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("chao")) //if (other.tag == "chao")
@@ -28,7 +28,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    
+
     void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("chao")) //if (other.tag == "chao")
@@ -36,28 +36,28 @@ public class Player : MonoBehaviour
             noChao = false;
         }
     }
-    
+
     void Update()
     {
-        
+
         Debug.Log("No Chao: " + noChao);
-        
-        if(Input.GetKey(KeyCode.LeftArrow))
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            _transform.position -= new Vector3(velocidade*Time.deltaTime,0,0);
-           Debug.Log("LeftArrow");
+            _transform.position -= new Vector3(velocidade * Time.deltaTime, 0, 0);
+            Debug.Log("LeftArrow");
         }
 
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-           _transform.position += new Vector3(velocidade*Time.deltaTime,0,0);
-           Debug.Log("RightArrow");
+            _transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+            Debug.Log("RightArrow");
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
-           // forcaPulo
-           _rigidbody2D.AddForce(new Vector2(0,forcaPulo),ForceMode2D.Impulse);
+            // forcaPulo
+            _rigidbody2D.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
         }
     }
 }
